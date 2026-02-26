ed678d110f0da41cf4cda095b3fc883925647f1d
JesusGaitan <JesusGaitan@DESKTOP-2GC2GAT>
2026-02-26 15:54:37 -0500

    Agregar archivos de proyecto.

---
+++ C_Semilleto/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(1234 + 56789);
+Console.WriteLine(1234 - 56789);
+Console.WriteLine(1234 * 56789);
+Console.WriteLine(1234 / 56789);
+Console.WriteLine(1234.0 / 56789);
