f699e0a3364792f1f531126389948e494d0fffa8
Jesus Gaitan <jgaitan345@gmail.com>
2026-02-26 16:29:09 -0500

    ejercicio1.0.5

---
+++ C_Semillero_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_Semillero_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Ingrese el primer número:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo número");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El resultado de división entera es: " + (a / b));
+            Console.WriteLine(" y su residuo es: " + (a % b));  
+
+        }
+    }
+}
