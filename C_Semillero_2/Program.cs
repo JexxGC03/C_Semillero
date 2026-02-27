6f37ff41110282d161700604f0954640ec9fba26
Jesus Gaitan <jgaitan345@gmail.com>
2026-02-27 09:51:00 -0500

    ejercicio2-0.4

--- C_Semillero_2/Program.cs
+++ C_Semillero_2/Program.cs
@@ -5,37 +5,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        int a;
-        Console.Write("Ingresa un número de la semana: ");
+        int a, b, cantidadDePsitivos;
+        Console.Write("Ingresa un número cualquiera: ");
         a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingresa un segundo número: ");
+        b = Convert.ToInt32(Console.ReadLine());
 
-        switch (a) {
-            case 1:
-                Console.WriteLine("Lunes");
-                break;
-            case 2:
-                Console.WriteLine("Martes");
-                break;
-            case 3:
-                Console.WriteLine("Miércoles");
-                break;
-            case 4:
-                Console.WriteLine("Jueves");
-                break;
-            case 5:
-                Console.WriteLine("Viernes");
-                break;
-            case 6:
-                Console.WriteLine("Sábado");
-                break;
-            case 7:
-                Console.WriteLine("Domingo");
-                break;
-            default:
-                Console.WriteLine("No existe un número de la semana con ese valor");
-                break;
-        }
+        cantidadDePsitivos = (a >= 0 && b >= 0) ? 2 : ((a < 0 && b < 0) ? 0 : 1);
+        Console.WriteLine("La catidad de positivos escritos es: {0}", cantidadDePsitivos);
 
-        
+
     }
 }
