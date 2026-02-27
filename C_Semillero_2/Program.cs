4f190a3d61fc4f7672c89d8f64c3b21207e944d1
Jesus Gaitan <jgaitan345@gmail.com>
2026-02-27 08:28:07 -0500

    ejercicio2.0.2

--- C_Semillero_2/Program.cs
+++ C_Semillero_2/Program.cs
@@ -5,17 +5,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        int a, b;
+        int a, b, c;
 
-        Console.WriteLine("Ingresa dos númeror para comparar");
+        Console.WriteLine("Ingresa los siguientes números para comparar");
         Console.Write("Ingresa el primer número: ");
         a = Convert.ToInt32(Console.ReadLine());
         Console.Write("Ingresa el segundo número: ");
         b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingresa el tercer número: ");
+        c = Convert.ToInt32(Console.ReadLine());
 
-        if (a >= b)
+        if (a > b && a > c)
             Console.WriteLine("El mayor es {0}", a);
-        else
+        else if (b > c)
             Console.WriteLine("El mayor es {0}", b);
+        else 
+            Console.WriteLine("El mayor es {0}", c);
     }
 }
