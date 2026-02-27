b0155a4e6c16544b485257680d72b28be381739c
Jesus Gaitan <jgaitan345@gmail.com>
2026-02-27 10:28:48 -0500

    ejercicio2.0.7

--- C_Semillero_2/Program.cs
+++ C_Semillero_2/Program.cs
@@ -7,13 +7,14 @@ class Program
     {
         int a = 0;
 
-        while (a != 1234)
+        do
         {
             Console.Write("Digite la contraseña: ");
             a = Convert.ToInt32(Console.ReadLine());
-            if (a != 1234 ) 
+            if (a != 1234)
                 Console.WriteLine("Contraseña Incorrecta");
         }
+        while (a != 1234);
 
         Console.WriteLine("Bienvenido");
     }
