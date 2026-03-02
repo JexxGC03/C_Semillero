f7373b2705dd64f8506aee87b3467390e693e148
Jesus Gaitan <jgaitan345@gmail.com>
2026-03-02 14:54:31 -0500

    ejercicio3.10

--- C_Semillero_2/Program.cs
+++ C_Semillero_2/Program.cs
@@ -6,33 +6,38 @@ class Program
     static void Main(string[] args)
     {
 
-        //Pide los 2 catetos de un tringulo rectangulo y le devuelve la hipotenusa
-        double a, b, hipot;
-        Console.WriteLine("Digite el valor del primer cateto: ");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Digite el valor del segundo cateto: ");
-        b = Convert.ToInt32(Console.ReadLine());
-        hipot = Math.Sqrt((Math.Pow(a, 2)) + (Math.Pow(b, 2)));
-        Console.WriteLine("La hipotenusa del tringulo es {0}", hipot);
+        //Mini traductor de C# a Java
+        Console.WriteLine("Introduce una instrucción de C# (if, while, for, Console.WriteLine):");
+        Console.Write("> ");
 
+        string entrada = Console.ReadLine().Trim();
 
-        //Pide un número y devuelve raiz cubica, elevado a la 1/3
-        double num;
-        Console.WriteLine("Digite un número: ");
-        num = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("La raíz cubica es  {0}", Math.Pow(num, 1/3));
+        switch (entrada)
+        {
+            case "if":
+                Console.WriteLine("Equivalencia en Java: if (condición) { ... }");
+                break;
 
-        //Pide un angulo y devuelve el Seno, Coseno y Tangente
-        Console.Write("Introduce un ángulo en grados: ");
-        double grados = Convert.ToDouble(Console.ReadLine());
+            case "while":
+                Console.WriteLine("Equivalencia en Java: while (condición) { ... }");
+                break;
 
-        double radianes = grados * (Math.PI / 180.0);
-        double seno = Math.Sin(radianes);
-        double coseno = Math.Cos(radianes);
-        double tangente = Math.Tan(radianes);
+            case "for":
+                Console.WriteLine("Equivalencia en Java: for (int i=0; i<n; i++) { ... }");
+                break;
 
-        Console.WriteLine("Para un angulo de {0} el Coseno es {1}, Seno {2}, Tangente {3}",
-            grados, coseno, seno, tangente);
+            case "Console.WriteLine":
+                Console.WriteLine("Equivalencia en Java: System.out.println();");
+                break;
 
+            default:
+                Console.WriteLine(" Error: Orden no reconocida o no soportada actualmente.");
+                break;
+        }
+
+        Console.WriteLine("\nPresiona cualquier tecla para salir...");
+        Console.ReadKey();
     }
+
 }
+
