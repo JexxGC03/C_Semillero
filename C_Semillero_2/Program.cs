4d94864387789427f8310bed030b12bb4399653f
Jesus Gaitan <jgaitan345@gmail.com>
2026-03-02 08:45:58 -0500

    ejercicio3.01

--- C_Semillero_2/Program.cs
+++ C_Semillero_2/Program.cs
@@ -5,27 +5,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        int a, b;
+        ushort year;
+        byte age;
+        uint poblation;
 
-        try
-        {
+        Console.Write("Digite su año de nacimiento: ");
+        year = Convert.ToUInt16(Console.ReadLine());
+        Console.Write("Digite su edad: ");
+        age = Convert.ToByte(Console.ReadLine());
+        Console.Write("Digite cuantos habitantes tiene su pueblo: ");
+        poblation = Convert.ToUInt32(Console.ReadLine());
 
-            Console.Write("Digite el primero número para la operación: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Digite el segundo número para la operación: ");
-            b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Su año de nacimiento es {0}, su edad es {1} y su pueblo tiene {2} cantidad de habitantes",
+            year, age, poblation);
 
-            Console.Write("La operación (A+B)/(A-B) es: ");
-            Console.WriteLine((a + b) / (a - b));
-        }
-        catch (FormatException)
-        {
-            Console.Write("Formato de número incorrecto");
-        }
-        catch (DivideByZeroException)
-        {
-            Console.WriteLine("Digite números diferentes, la resta de a - b es igual a 0");
-        }
-        Console.WriteLine("...");
     }
 }
