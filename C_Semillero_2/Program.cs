a47f82fe5c5072baf81e4b4fe99831694e9d5d6c
Jesus Gaitan <jgaitan345@gmail.com>
2026-03-02 08:18:40 -0500

    ejercicio.2.0.12

--- C_Semillero_2/Program.cs
+++ C_Semillero_2/Program.cs
@@ -5,93 +5,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Número del 1 al 200 que también son multiplos de 3 y 7
-        for (int i = 1; i <= 200; i++) 
+        int a, b;
+
+        try
         {
-            int m3 = i % 3;
-            int m7 = i % 7;
 
-            if (m3 == 0)
-                Console.WriteLine("Multiplo de 3: {0}", i);
+            Console.Write("Digite el primero número para la operación: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite el segundo número para la operación: ");
+            b = Convert.ToInt32(Console.ReadLine());
 
-            if (m7 == 0)
-                Console.WriteLine("Multiplo de 7: {0}", i);
+            Console.Write("La operación (A+B)/(A-B) es: ");
+            Console.WriteLine((a + b) / (a - b));
         }
-
-
-        // Pide número hasta que escriba 0
-        int num;
-        do
+        catch (FormatException)
         {
-            Console.Write("Escribe cualquier número: ");
-            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Formato de número incorrecto");
         }
-        while (num != 0);
-
-        //Muestra divisores de un número ingresado
-        Console.Write("Escribe un número para imprimir sus divisores: ");
-        num = Convert.ToInt32(Console.ReadLine());
-        for (int i = 1;  i <= num; i++)
+        catch (DivideByZeroException)
         {
-            if ((num % i) == 0)
-                Console.WriteLine("{0} es divisor de {1}", i, num); 
+            Console.WriteLine("Digite números diferentes, la resta de a - b es igual a 0");
         }
-
-        //Pregunta un número y dice si es primo
-        Console.Write("Introduce un número para verificar si es primo: ");
-        if (int.TryParse(Console.ReadLine(), out int numero))
-        {
-            bool esPrimo = true;
-            if (numero < 2)
-                esPrimo = false;
-            else if (numero == 2)
-                esPrimo = true;
-            else if (numero % 2 == 0)
-                esPrimo = false; 
-            else
-            {
-                double limite = Math.Sqrt(numero);
-                for (int i = 3; i <= limite; i += 2)
-                {
-                    if (numero % i == 0)
-                    {
-                        esPrimo = false;
-                        break; 
-                    }
-                }
-            }
-
-            // Resultado final
-            if (esPrimo)
-                Console.WriteLine($"El número {numero} ES primo.");
-            else
-                Console.WriteLine($"El número {numero} NO es primo.");
-        }
-        else
-        {
-            Console.WriteLine("Por favor, introduce un número entero válido.");
-        }
-
-        //5 Oportunidades para advinar contraseña
-        int op = 5;
-        do
-        {
-            Console.WriteLine("Intente adivinar la contraseña: ");
-            String password;
-            password = Console.ReadLine();
-
-            if (password != "12345")
-            { 
-                op--;
-                Console.WriteLine("Incorrecto, tiene {0} oportunidades más.", op);
-            }
-            else
-            {
-                op = 0;
-                Console.WriteLine("Adivino la Contraseña");
-            }
-
-
-        } while (op >= 1);
+        Console.WriteLine("...");
     }
 }
