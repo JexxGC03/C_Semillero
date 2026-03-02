dcc2a463f50b8d014952d704f4cf99fe55a48a92
Jesus Gaitan <jgaitan345@gmail.com>
2026-03-02 12:41:11 -0500

    ejercicio3.07

--- C_Semillero_2/Program.cs
+++ C_Semillero_2/Program.cs
@@ -6,30 +6,33 @@ class Program
     static void Main(string[] args)
     {
 
-        //Saca la superficie de un circulo con 2 decimales
-        double pi = 3.141596;
-        double radio, superficie;
+        //Pide los 2 catetos de un tringulo rectangulo y le devuelve la hipotenusa
+        double a, b, hipot;
+        Console.WriteLine("Digite el valor del primer cateto: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite el valor del segundo cateto: ");
+        b = Convert.ToInt32(Console.ReadLine());
+        hipot = Math.Sqrt((Math.Pow(a, 2)) + (Math.Pow(b, 2)));
+        Console.WriteLine("La hipotenusa del tringulo es {0}", hipot);
 
-        Console.Write("Digite el radio de un circulo para calcular la superficie: ");
-        radio = Convert.ToDouble(Console.ReadLine());
-        superficie = (pi * (radio*radio));
-        Console.WriteLine("La suoerficie de un circulo con radio {0} es: {1}",
-            radio, superficie.ToString("0.00"));
 
+        //Pide un número y devuelve raiz cubica, elevado a la 1/3
+        double num;
+        Console.WriteLine("Digite un número: ");
+        num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("La raíz cubica es  {0}", Math.Pow(num, 1/3));
 
-        //Pide un número y lo convierte a Hexa y Binario
-        int num;
-        do
-        {
-            int a, b;
-            Console.Write("Digita un número: ");
-            num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Tu número en hexadecimal es: {0} y en binario es: {1} ",
-                Convert.ToString(num, 16),
-                Convert.ToString(num, 2));
-        }
-        while (num != 0);
+        //Pide un angulo y devuelve el Seno, Coseno y Tangente
+        Console.Write("Introduce un ángulo en grados: ");
+        double grados = Convert.ToDouble(Console.ReadLine());
 
+        double radianes = grados * (Math.PI / 180.0);
+        double seno = Math.Sin(radianes);
+        double coseno = Math.Cos(radianes);
+        double tangente = Math.Tan(radianes);
+
+        Console.WriteLine("Para un angulo de {0} el Coseno es {1}, Seno {2}, Tangente {3}",
+            grados, coseno, seno, tangente);
 
     }
 }
